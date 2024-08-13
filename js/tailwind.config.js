367d43893c57db486396529ad0acdd1f6745fa53
rakhmatovdoniyordev <rakhmatovdoniyordev@gmail.com>
2024-08-14 03:12:36 +0500

    tailwind

---
+++ js/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+      extend: {
+        boxShadow: {
+          'custom': '34.85px 29.63px 48.34px 0px #3366FF0D',
+        },
+      },
+    },
+    plugins: [],
+  }
